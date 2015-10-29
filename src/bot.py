1874ccfc8bb5bdb0cec2bbc269e1812c2bfd3b31
Kristoffer Johansen <krij12@student.uia.no>
2015-10-29 19:49:42 +0100

    Added temperature conversion and a kill command.

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,60 @@
+# -*- coding: utf-8 -*-
 import communicator
+import math
+import pprint
+import sys
+
+
+def convtemperature(scale1, scale2, value):
+    funcvalid = scale1 is not None and scale2 is not None and value is not None
+    if not funcvalid:
+        return "Not enough parameters. The command is !temp [scale1] [scale2] [value to convert]."
+
+    try:
+        value = float(value)
+    except ValueError:
+        return "Given value was invalid. Please try again."
+
+    scale1 = str.upper(scale1)
+    scale2 = str.upper(scale2)
+    convvalue = value
+    scale1valid = True
+    scale2valid = True
+
+    if scale1 == "C":
+        if scale2 == "K":
+            convvalue = value + 273.15
+        elif scale2 == "F":
+            convvalue = (value * 1.8) + 32
+        else:
+            scale2valid = False
+
+    elif scale1 == "F":
+        if scale2 == "K":
+            convvalue = (value + 459.67) * 5/9
+        elif scale2 == "C":
+            convvalue = (value - 32) * 5/9
+        else:
+            scale2valid = False
+
+    elif scale1 == "K":
+        if scale2 == "C":
+            convvalue = value - 273.15
+        elif scale2 == "F":
+            convvalue = (value * 1.8) - 459.67
+        else:
+            scale2valid = False
+
+    else:
+        scale1valid = False
+
+    if scale2valid is False:
+        return "Chosen scale was invalid."
+
+    if scale1valid is False:
+        return "Original scale was invalid."
+
+    return "{0:0.2f}° {1} is {2:0.2f}° {3}.".format(value, scale1, convvalue, scale2)
 
 
 def commands(nick, chan, msg):
@@ -8,8 +64,18 @@ def commands(nick, chan, msg):
         return
 
     command = msg.split(' ')[0][1:]
-    if command == "hai":
-        communicator.sendmsg(chan, "oh hai")
+    if command == "temp":
+        args = msg.split(' ')[1:]
+        if len(args) == 3:
+            communicator.sendmsg(chan, convtemperature(args[0], args[1], args[2]))
+        else:
+            communicator.sendmsg(chan, "Try using !temp [scale1] [scale2] [value to convert]")
+
+    elif command == "shutdown":
+        if str.lower(nick) == "krazy6446" or str.lower(nick) == "krazeh":
+            sys.exit(0)
+        else:
+            communicator.sendmsg(chan, "Nope.")
 
 
 while 1:
